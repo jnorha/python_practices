3ba4c45eedf64dc755fd07e85737d65a2aed00fd
jnorha <45795929+jnorha@users.noreply.github.com>
2020-05-13 10:37:39 -0500

    Create string_hist.py

---
+++ string_hist.py
@@ -0,0 +1,17 @@
+# Creates a "Histogram" dictionary from a string (could be converted to xcel)
+
+def histogram(s):
+    d = dict()
+    for i in s:
+        val = d.get(i, 0)
+        d[i] = + 1
+    return d
+
+def get_input():
+    g = raw_input("What would you like to count the letters of? \n")
+    return g
+
+x = get_input()
+
+print histogram(x)
+print histogram("bananas")
